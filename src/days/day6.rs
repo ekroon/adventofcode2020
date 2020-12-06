198657438051865ee254cf7e6245151286f3fc00
Erwin Kroon <erwin@k3n.nl>
2020-12-06 14:32:08 +0400

    Day6: use integer logic

--- src/days/day6.rs
+++ src/days/day6.rs
@@ -1,43 +1,42 @@
 use aoc_runner_derive::aoc;
-use std::collections::HashSet;
 
 #[aoc(day6, part1)]
 pub fn part1(input: &str) -> Option<usize> {
     let mut sum = 0;
-    let mut questions: HashSet<char> = HashSet::new();
+    let mut questions = 0u32;
     for group in input.split("\n\n") {
-        for char in group.chars() {
-            if char != '\n' {
-                questions.insert(char);
+        for char in group.as_bytes() {
+            if *char != b'\n' {
+                questions |= 1 << (char - b'a')
             }
         }
-        sum += questions.len();
-        questions.clear();
+        sum += questions.count_ones();
+        questions = 0;
     }
-    Some(sum)
+    Some(sum as usize)
 }
 
 #[aoc(day6, part2)]
 pub fn part2(input: &str) -> Option<usize> {
     let mut sum = 0;
-    let mut questions: HashSet<char> = HashSet::new();
-    let mut person_questions: HashSet<char> = HashSet::new();
+    let mut questions = 0u32;
+    let mut person_questions = 0u32;
     for group in input.split("\n\n") {
         for (i, person) in group.lines().enumerate() {
-            for char in person.chars() {
-                if char != '\n' {
-                    person_questions.insert(char);
+            for char in person.as_bytes() {
+                if *char != b'\n' {
+                    person_questions |= 1 << (char - b'a');
                 }
             }
             if i == 0 {
-                questions.extend(&person_questions);
+                questions = person_questions;
             } else {
-                questions.retain(|q| person_questions.contains(q));
+                questions &= person_questions;
             }
-            person_questions.clear();
+            person_questions = 0;
         }
-        sum += questions.len();
-        questions.clear();
+        sum += questions.count_ones();
+        questions = 0;
     }
-    Some(sum)
+    Some(sum as usize)
 }

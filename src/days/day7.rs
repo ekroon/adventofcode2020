82eb7026b74bf26d45e1580e5768745ad34b4d7d
Erwin Kroon <erwin@k3n.nl>
2020-12-07 15:08:55 +0400

    Day7: speedup

--- src/days/day7.rs
+++ src/days/day7.rs
@@ -1,21 +1,23 @@
 use aoc_runner_derive::aoc;
-use std::collections::{BTreeSet, HashMap, HashSet, VecDeque};
+use std::collections::{HashMap, HashSet, VecDeque};
 
-#[derive(Copy, Clone, Eq, PartialEq, Hash, Debug, Ord, PartialOrd)]
+#[derive(Copy, Clone, Eq, PartialEq, Hash, Debug, Ord, PartialOrd, Default)]
 pub struct Bag<'a> {
     prefix: &'a str,
     color: &'a str,
 }
 
-type FromTo<'a> = Vec<(Bag<'a>, Vec<(Bag<'a>, i32)>)>;
+const shiny_gold: Bag = Bag {
+    prefix: "shiny",
+    color: "gold",
+};
 
-pub fn parse(input: &str) -> Option<Vec<(Bag, Vec<(Bag, i32)>)>> {
-    let mut parsed: Vec<(Bag, Vec<(Bag, i32)>)> = vec![];
-    for line in input.lines() {
+pub fn parse(input: &str) -> impl Iterator<Item = (Bag, Vec<(i32, Bag)>)> {
+    input.lines().map(|line| {
         let mut words = line.split_ascii_whitespace();
         let bag = Bag {
-            prefix: words.next()?,
-            color: words.next()?,
+            prefix: words.next().unwrap(),
+            color: words.next().unwrap(),
         };
         words.next();
         words.next();
@@ -25,38 +27,44 @@ pub fn parse(input: &str) -> Option<Vec<(Bag, Vec<(Bag, i32)>)>> {
                 break;
             }
             bags.push((
-                Bag {
-                    prefix: words.next()?,
-                    color: words.next()?,
-                },
                 word.parse().unwrap(),
+                Bag {
+                    prefix: words.next().unwrap(),
+                    color: words.next().unwrap(),
+                },
             ));
             words.next();
         }
-        parsed.push((bag, bags));
-    }
-
-    Some(parsed)
+        (bag, bags)
+    })
 }
 
 #[aoc(day7, part1)]
 pub fn part1(input: &str) -> Option<usize> {
-    let parsed = parse(input)?;
-    let shiny_gold = Bag {
-        prefix: "shiny",
-        color: "gold",
-    };
+    let parsed = parse(input).fold(
+        HashMap::new(),
+        |mut acc: HashMap<Bag, Vec<Bag>>, (from, to)| {
+            for (_, to) in to {
+                if let Some(bags) = acc.get_mut::<Bag>(&to) {
+                    bags.push(from);
+                } else {
+                    let mut bags = vec![];
+                    bags.push(from);
+                    acc.insert(to, bags);
+                }
+            }
+            acc
+        },
+    );
 
     let mut queue = VecDeque::new();
     let mut seen_bags = HashSet::new();
     queue.push_back(shiny_gold);
     while let Some(bag) = queue.pop_front() {
         if seen_bags.insert(bag) {
-            for (from, to) in &parsed {
-                for (b, _) in to {
-                    if b == &bag {
-                        queue.push_back(*from);
-                    }
+            if let Some(bags) = parsed.get(&bag) {
+                for bag in bags {
+                    queue.push_back(*bag)
                 }
             }
         }
@@ -66,21 +74,18 @@ pub fn part1(input: &str) -> Option<usize> {
 
 #[aoc(day7, part2)]
 pub fn part2(input: &str) -> Option<i32> {
-    let parsed = parse(input)?;
-    let shiny_gold = Bag {
-        prefix: "shiny",
-        color: "gold",
-    };
+    let parsed = parse(input).fold(HashMap::new(), |mut acc, (from, to)| {
+        acc.insert(from, to);
+        acc
+    });
 
     let mut queue = VecDeque::new();
     let mut count = 0;
     queue.push_back((shiny_gold, 1));
     while let Some((bag, num)) = queue.pop_front() {
         count += num;
-        for (from, to) in &parsed {
-            if *from == bag {
-                to.iter().for_each(|i| queue.push_back((i.0, i.1 * num)));
-            }
+        if let Some(to) = parsed.get(&bag) {
+            to.iter().for_each(|i| queue.push_back((i.1, i.0 * num)));
         }
     }
     Some(count - 1)

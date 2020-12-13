0c381397ce859d1d64f89f3dbbba8a5b90bec539
Erwin Kroon <erwin@k3n.nl>
2020-12-13 14:01:29 +0400

    Day13: parse as bytes

--- src/days/day13.rs
+++ src/days/day13.rs
@@ -1,15 +1,17 @@
 use aoc_runner_derive::aoc;
 use itertools::Itertools;
+use lexical::parse;
 
 #[aoc(day13, part1)]
 pub fn part1(input: &str) -> Option<usize> {
-    let mut lines = input.lines();
-    let earliest_depart = lines.next().unwrap().parse::<usize>().unwrap();
-    let bus_ids = lines
+    let mut bytes = input.as_bytes().split(|b| *b == b'\n');
+
+    let earliest_depart = parse::<usize, _>(bytes.next().unwrap()).unwrap();
+    let bus_ids = bytes
         .next()
         .unwrap()
-        .split(',')
-        .flat_map(&str::parse)
+        .split(|b| *b == b',')
+        .flat_map(parse)
         .collect::<Vec<usize>>();
     let mut min_wait = usize::MAX;
     let mut pick = 0usize;
@@ -25,26 +27,25 @@ pub fn part1(input: &str) -> Option<usize> {
 
 #[aoc(day13, part2)]
 pub fn part2(input: &str) -> Option<usize> {
-    let mut lines = input.lines();
-    lines.next(); // drop first
-    let mut bus_ids = lines
+    let mut bytes = input.as_bytes().split(|b| *b == b'\n');
+    bytes.next(); // drop first
+    let mut bus_ids = bytes
         .next()
         .unwrap()
-        .split(',')
+        .split(|b| *b == b',')
         .enumerate()
         .filter_map(|s| {
-            if let Ok(n) = s.1.parse::<usize>() {
+            if let Ok(n) = parse(s.1) {
                 (s.0, n).into()
             } else {
                 None
             }
         })
-        .sorted_by_key(|v| v.1)
-        .rev();
+        .sorted_by(|v1, v2| Ord::cmp(&v2.1, &v1.1));
 
     let (index, id) = bus_ids.next().unwrap();
     let mut step_size = id;
-    let mut start_at = id - index;
+    let mut start_at = id - index % id;
     for (index, id) in bus_ids {
         for n in (start_at..).step_by(step_size) {
             if (n + index) % id == 0 {
